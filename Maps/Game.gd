b536031a5f6657a24fc410d2abf249c28af290d4
Daniel Amado <velkan14@gmail.com>
2019-10-28 16:34:30 +0000

    Refactor of the transition from doors and rooms

--- Maps/Game.gd
+++ Maps/Game.gd
@@ -6,93 +6,57 @@ onready var player = null
 var start_scene = preload("res://Maps/Moki Town/HeroHome.tscn")
 var current_scene = null
 
-var next_scene2 = null
-var next_scene3 = null
-var next_scene4 = null
-
 var isInteracting = false
 var canInteract = true
+
+onready var transition = $CanvasLayer/Transition
 
 func _ready():
 	player = Player.instance()
 	add_child(player)
-	
-	current_scene = start_scene.instance()
-	add_child(current_scene)
+	change_scene(start_scene)
 	
 	player.position = Vector2(Global.TrainerX, Global.TrainerY)
 	player.z_index = 8
 
-func transition_visibility():
-	$CanvasLayer/Node2D.visible = !$CanvasLayer/Node2D.visible
-
-func play_anim(fade):
-	$CanvasLayer/Node2D/AnimationPlayer.play(fade)
-
-func room_transition(dir):
-	player.disable_input()
-	
-	transition_visibility()
-	play_anim("fade_in")
-	yield(get_tree().create_timer(0.28), "timeout")
-	
-	if dir == "Up":
-		$Map/Floor2/DownStairs/CollisionShape2D.disabled = true
-		Global.TrainerX = 64
-		Global.TrainerY = 80
-	elif dir == "Down":
-		$Map/Floor1/UpStairs/CollisionShape2D.disabled = true
-		Global.TrainerX = 1184
-		Global.TrainerY = 80
-	
-	if dir == "Up":
-		player.direction = 2
-	elif dir == "Down":
-		player.direction = 1
-	
-	player.position = Vector2(Global.TrainerX, Global.TrainerY)
-	player.movePrevious()
-	yield(get_tree().create_timer(0.3), "timeout")
-	play_anim("fade_out")
-	
-	
-	player.move()
-	player.movePrevious()
-	player.inputDisabled = true
-	yield(get_tree().create_timer(0.3), "timeout")
-	
-	if dir == "Up":
-		$Map/Floor2/DownStairs/CollisionShape2D.disabled = false
-	elif dir == "Down":
-		$Map/Floor1/UpStairs/CollisionShape2D.disabled = false
-	
-	player.enable_input()
-	transition_visibility()
-
-func door_transition(scene):
-	player.disable_input()
-	
-	transition_visibility()
-	
-	play_anim("fade_in")
-	yield(get_tree().create_timer(0.28), "timeout")
-	remove_child($Map)
+func change_scene(scene):
+	if current_scene:
+		remove_child(current_scene)
 	current_scene = scene.instance()
-	
 	add_child(current_scene)
-	player.position = Vector2(get_child(2).hero_home_x, get_child(2).hero_home_y)
-	yield(get_tree().create_timer(0.3), "timeout")
-	player.direction = 0
+	for node in get_tree().get_nodes_in_group("transition"):
+		node.initialize(self)
+	
+func room_transition(new_position):
+	player.disable_input()
+	yield(transition.fade_to_color(), "completed")
+	
+	Global.TrainerX = new_position.x
+	Global.TrainerY = new_position.y
+	player.position = new_position
 	player.movePrevious()
+	yield(transition.fade_from_color(), "completed")
+	
 	player.move()
-	play_anim("fade_out")
-	yield($CanvasLayer/Node2D/AnimationPlayer, "animation_finished")
 	player.movePrevious()
-	transition_visibility()
-	
 	player.enable_input()
+
+func door_transition(path_scene, new_position):
+	player.disable_input()
+	yield(transition.fade_to_color(), "completed")
 	
-	pass
+	change_scene(load(path_scene))
+	
+	Global.TrainerX = new_position.x
+	Global.TrainerY = new_position.y
+	player.position = new_position
+	#player.direction = 0
+	player.movePrevious()
+	yield(transition.fade_from_color(), "completed")
+	
+	player.move()
+	player.movePrevious()
+	player.enable_input()
 
 func interaction(collider):
 	if isInteracting == true or !canInteract:
@@ -100,7 +64,6 @@ func interaction(collider):
 	isInteracting = true
 	canInteract = false
 	
-	print($Map/Floor2/TV.position)
 	if collider == $Map/Floor2/Console.position:
 		player.disable_input()
 		get_child(2).consoleDialoge()
