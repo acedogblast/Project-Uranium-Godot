68d3ade9137839c18a3dceae192f2d82011464db
5H4D0WILA <kempcompany05@gmail.com>
2019-11-02 17:15:15 +0100

    Working on Route 03

--- Utilities/Stairs.gd
+++ Utilities/Stairs.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 export var destination = Vector2()
+export var dir = ""
 var game 
 
 func initialize(_game):
@@ -8,5 +9,5 @@ func initialize(_game):
 
 func _on_Stairs_area_shape_entered(area_id, area, area_shape, self_shape):
 	$CollisionShape2D.disabled = true
-	game.room_transition(destination)
+	game.room_transition(destination, dir)
 	$CollisionShape2D.disabled = false
