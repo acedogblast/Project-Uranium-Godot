5a2bb9931533bbdd3de08946368e0c35e580a7bc
ShadowTheR34l <kempcompany05@gmail.com>
2020-01-13 23:37:29 +0100

    .gitignore changed

--- Maps/TiledMaps/Routes/Route1.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-const SEEMLESS = false
-
-onready var nextMap = preload("res://Maps/TiledMaps/Towns/Kevlar Town.tmx")
-onready var lastMap = preload("res://Maps/MokiTown/MokiTown.tscn")
--- Maps/TiledMaps/Tilesets/PU_caves.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="PU_caves" tilewidth="16" tileheight="16" tilecount="884" columns="26">
- <image source="../../tilesets/PU_caves.png" width="416" height="544"/>
-</tileset>
