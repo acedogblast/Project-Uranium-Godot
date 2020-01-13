58685d1431d4ab245c3e399e5c839135f803f260
ShadowTheR34l <kempcompany05@gmail.com>
2020-01-13 23:25:56 +0100

    Event System

---
+++ Maps/TiledMaps/Routes/Route1.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+const SEEMLESS = false
+
+onready var nextMap = preload("res://Maps/TiledMaps/Towns/Kevlar Town.tmx")
+onready var lastMap = preload("res://Maps/MokiTown/MokiTown.tscn")
---
+++ Maps/TiledMaps/Tilesets data/PU_groundtiles+.tsx
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="PU_groundtiles+" tilewidth="16" tileheight="16" tilecount="1536" columns="32">
+ <image source="../../tilesets/PU_groundtiles+.png" width="512" height="768"/>
+ <terraintypes>
+  <terrain name="Grassy Path" tile="129"/>
+  <terrain name="Dirt Path" tile="225"/>
+  <terrain name="Grassy Steet" tile="321"/>
+  <terrain name="Street" tile="321"/>
+  <terrain name="Fall Leaf" tile="705"/>
+  <terrain name="Brick Path" tile="801"/>
+  <terrain name="Brick Path 2" tile="1249"/>
+  <terrain name="Mountain Path" tile="1249"/>
+ </terraintypes>
+ <tile id="30">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index">
+   <object id="5" x="16" y="16">
+    <polygon points="0,0 0,-16 -16,-16 -16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index">
+   <object id="7" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="96" terrain=",,,0"/>
+ <tile id="97" terrain=",,0,0"/>
+ <tile id="98" terrain=",,0,"/>
+ <tile id="99" terrain=",0,0,0"/>
+ <tile id="100" terrain="0,,0,0"/>
+ <tile id="128" terrain=",0,,0"/>
+ <tile id="129" terrain="0,0,0,0"/>
+ <tile id="130" terrain="0,,0,"/>
+ <tile id="131" terrain="0,0,,0"/>
+ <tile id="132" terrain="0,0,0,"/>
+ <tile id="160" terrain=",0,,"/>
+ <tile id="161" terrain="0,0,,"/>
+ <tile id="162" terrain="0,,,"/>
+ <tile id="192" terrain=",,,1"/>
+ <tile id="193" terrain=",,1,1"/>
+ <tile id="194" terrain=",,1,"/>
+ <tile id="195" terrain="1,1,1,"/>
+ <tile id="196" terrain="1,1,,1"/>
+ <tile id="224" terrain=",1,,1"/>
+ <tile id="225" terrain="1,1,1,1"/>
+ <tile id="226" terrain="1,,1,"/>
+ <tile id="227" terrain="1,,1,1"/>
+ <tile id="228" terrain=",1,1,1"/>
+ <tile id="256" terrain=",1,,"/>
+ <tile id="257" terrain="1,1,,"/>
+ <tile id="258" terrain="1,,,"/>
+ <tile id="259" terrain="2,2,2,"/>
+ <tile id="260" terrain="2,2,,2"/>
+ <tile id="288" terrain=",,,2"/>
+ <tile id="289" terrain=",,2,2"/>
+ <tile id="290" terrain=",,2,"/>
+ <tile id="291" terrain="2,,2,2"/>
+ <tile id="292" terrain=",2,2,2"/>
+ <tile id="311" terrain=",7,7,7"/>
+ <tile id="312" terrain="7,,7,7"/>
+ <tile id="313" terrain=",,,7"/>
+ <tile id="314" terrain=",,7,7"/>
+ <tile id="315" terrain=",,7,"/>
+ <tile id="320" terrain=",2,,2"/>
+ <tile id="322" terrain="2,,2,"/>
+ <tile id="323" terrain=",3,3,3"/>
+ <tile id="324" terrain="3,,3,3"/>
+ <tile id="343" terrain="7,7,,7"/>
+ <tile id="344" terrain="7,7,7,"/>
+ <tile id="345" terrain=",7,,7"/>
+ <tile id="346" terrain="7,7,7,7"/>
+ <tile id="347" terrain="7,,7,"/>
+ <tile id="352" terrain=",2,,"/>
+ <tile id="353" terrain="2,2,,"/>
+ <tile id="354" terrain="2,,,"/>
+ <tile id="355" terrain="3,3,,3"/>
+ <tile id="356" terrain="3,3,3,"/>
+ <tile id="376">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="377" terrain=",7,,"/>
+ <tile id="378" terrain="7,7,,"/>
+ <tile id="379" terrain="7,,,"/>
+ <tile id="384" terrain=",,,3"/>
+ <tile id="385" terrain=",,3,3"/>
+ <tile id="386" terrain=",,3,"/>
+ <tile id="416" terrain=",3,,3"/>
+ <tile id="417" terrain="3,3,3,3"/>
+ <tile id="418" terrain="3,,3,"/>
+ <tile id="448" terrain=",3,,"/>
+ <tile id="449" terrain="3,3,,"/>
+ <tile id="450" terrain="3,,,"/>
+ <tile id="672" terrain=",,,4"/>
+ <tile id="673" terrain=",,4,4"/>
+ <tile id="674" terrain=",,4,"/>
+ <tile id="675" terrain=",4,4,4"/>
+ <tile id="676" terrain="4,,4,4"/>
+ <tile id="704" terrain=",4,,4"/>
+ <tile id="705" terrain="4,4,4,4"/>
+ <tile id="706" terrain="4,,4,"/>
+ <tile id="707" terrain="4,4,,4"/>
+ <tile id="708" terrain="4,4,4,"/>
+ <tile id="736" terrain=",4,,"/>
+ <tile id="737" terrain="4,4,,"/>
+ <tile id="738" terrain="4,,,"/>
+ <tile id="768" terrain=",,,5"/>
+ <tile id="769" terrain=",,5,5"/>
+ <tile id="770" terrain=",,5,"/>
+ <tile id="771" terrain=",5,5,5"/>
+ <tile id="772" terrain="5,,5,5"/>
+ <tile id="800" terrain=",5,,5"/>
+ <tile id="801" terrain="5,5,5,5"/>
+ <tile id="802" terrain="5,,5,"/>
+ <tile id="803" terrain="5,5,,5"/>
+ <tile id="804" terrain="5,5,5,"/>
+ <tile id="832" terrain=",5,,"/>
+ <tile id="833" terrain="5,5,,"/>
+ <tile id="834" terrain="5,,,"/>
+ <tile id="1187" terrain=",6,6,6"/>
+ <tile id="1188" terrain="6,,6,6"/>
+ <tile id="1216" terrain=",,,6"/>
+ <tile id="1217" terrain=",,6,6"/>
+ <tile id="1218" terrain=",,6,"/>
+ <tile id="1219" terrain="6,6,,6"/>
+ <tile id="1220" terrain="6,6,6,"/>
+ <tile id="1248" terrain=",6,,6"/>
+ <tile id="1249" terrain="6,6,6,6"/>
+ <tile id="1250" terrain="6,,6,"/>
+ <tile id="1280" terrain=",6,,"/>
+ <tile id="1281" terrain="6,6,,"/>
+ <tile id="1282" terrain="6,,,"/>
+</tileset>
