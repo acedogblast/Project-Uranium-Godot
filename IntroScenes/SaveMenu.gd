2a5b333c62134bfd6d19261c73651b3f32ce85c8
Jasmine Bugielski <doyoulikejas64@gmail.com>
2020-05-07 20:35:45 -0400

    Addded comments to Menu.gd, SaveMenu.gd, and Start.gd

--- IntroScenes/SaveMenu.gd
+++ IntroScenes/SaveMenu.gd
@@ -22,14 +22,16 @@ enum SELECTED { # for the full pannel
 var selected = 0
 var save_id = 1
 func _ready():
-	# Check for existing save files
+	#Gets the number of saves from the save system
 	var num = SaveSystem.get_number_of_saves()
+	#If there are no saves, then set the panel layout to panel.new, set the panels to visible, set the FullPanel to invisible, uses the greenbox texture region for a new game, and greybox for the exit
 	if num == 0:
 		pannel_layout = PANNEL.NEW
 		$Panels.visible = true
 		$FullPanel.visible = false
 		$Panels/NewGame.texture.region = greenBox
 		$Panels/Exit.texture.region = grayBox
+	#If there are saves, then set the panel layout to panel.full, set the panels to invisible, make the fullpanel visible, and use greenload for the fullpanel region_rect
 	else:
 		pannel_layout = PANNEL.FULL
 		$Panels.visible = false
@@ -41,27 +43,34 @@ func _ready():
 
 
 func _process(delta):
+	#If panel layout is pannel.new then run the next line
 	if pannel_layout == PANNEL.NEW:
+		#If the selected is 0, and down is pressed, then select one and call the updateboxes method
 		if Input.is_action_just_pressed("ui_down") && selected == 0:
 			selected = 1
 			updateBoxes()
+		#If the selected is 1, and up is pressed, then select zero and call the updateboxes mehtod
 		if Input.is_action_just_pressed("ui_up") && selected == 1:
 			selected = 0
 			updateBoxes()
 		
-		#Selection choices
+		#If accept is pressed while 0 is selected, call the newgame method
 		if Input.is_action_just_pressed("ui_accept") && selected == 0:
 			NewGame()
+		#If accept is pressed while 1 is selected, get the tree and call the quit method
 		if Input.is_action_just_pressed("ui_accept") && selected == 1:
 			get_tree().quit()
 		pass
+	#If the panel layout is not pannel.new, run the next line
 	else:
+		#If down is pressed and the selected is exit, then set the selected to continue. If not, set the seleced to itself + 1
 		if Input.is_action_just_pressed("ui_down"):
 			if selected == SELECTED.EXIT:
 				selected = SELECTED.CONTINUE
 			else:
 				selected += 1
 			updateBoxes()
+		#If up is pressed and the selected is continue, then set the selected to exit. if not, set the slected to itself minus 1
 		if Input.is_action_just_pressed("ui_up"):
 			if selected == SELECTED.CONTINUE:
 				selected = SELECTED.EXIT
@@ -80,16 +89,20 @@ func _process(delta):
 					get_tree().quit()
 		pass
 	
+#Changes the scene to character creation
 func NewGame():
 	changeScene("res://IntroScenes/PlayerCreation.tscn")
 	pass
 	
 func updateBoxes():
+	#If the pannel layout is PANNEL.NEW play the audio stream player and go to the next line
 	if pannel_layout == PANNEL.NEW:
+		#If the selected is 0, then set the newgame texture region to greenbox, and the exit texture region to graybox
 		if selected == 0:
 			$Panels/NewGame.texture.region = greenBox
 			$Panels/Exit.texture.region = grayBox
 			
+		#If the selected is 1, then set the newgame texture region to graybox, and the exit texture region to greenbox
 		if selected == 1:
 			$Panels/NewGame.texture.region = grayBox
 			$Panels/Exit.texture.region = greenBox
@@ -131,6 +144,8 @@ func updateBoxes():
 		else:
 			$FullPanel.position = Vector2(50, -160)
 		$AudioStreamPlayer.play()
+		
+#Changes loads the method newscene in the parent node if on mobile, or gets change scene from the scene tree if not on mobile
 func changeScene(scene):
 	if Global.isMobile:
 		get_parent().newScene(scene)
@@ -138,6 +153,7 @@ func changeScene(scene):
 		get_tree().change_scene(scene)
 	pass
 
+#Loads the map to continue your game
 func continueGame(id):
 	Global.load_game_from_id = id
 	changeScene("res://Maps/Game.tscn")
