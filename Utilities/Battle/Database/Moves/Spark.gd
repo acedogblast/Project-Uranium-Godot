2de56f929abe1a4428a39227b6cdaa9da2b04aeb
Enerjy253 <55967282+Enerjy253@users.noreply.github.com>
2021-06-26 16:41:49 -0400

    Update Spark.gd

--- Utilities/Battle/Database/Moves/Spark.gd
+++ Utilities/Battle/Database/Moves/Spark.gd
@@ -1,16 +1,16 @@
 extends Object
 
 # The name of the move
-var name = "Double_Team"
+var name = "Spark"
 
 # The type of the move
-var type = Type.NORMAL
+var type = Type.ELECTRIC
 
 # The style of the move (Physical, Special, Status)
-var style = MoveStyle.STATUS
+var style = MoveStyle.PHYSICAL
 
 # The base power of the move
-var base_power
+var base_power = 65
 
 # The accuracy of the move
 var accuracy = 100
@@ -19,18 +19,21 @@ var accuracy = 100
 var priority = 0
 
 # The critical hit level of the move 1=6.25% 2=12.5% 3=25% 4=33.3% 5=50% 
-var critical_hit_level = 0
+var critical_hit_level = 1
 
 # The secondary effect chance of the move
-var secondary_effect_chance 
+var secondary_effect_chance = 0.3
+
+# The secondary effect of the move
+var secondary_effect = MajorAilment.PARALYZE
 
 # The flags of the move
 var flags = []
 
 # The total pp of the move
-var total_pp = 15
+var total_pp = 20
 
 # The target ability of the move (Single, Double, All_Foes, Self)
 var target_ability = MoveTarget.SELF
 
-var main_status_effect = StatStageEffect.new(0, 0, 0, 0, 0, 0, 1)
+var main_status_effect 
