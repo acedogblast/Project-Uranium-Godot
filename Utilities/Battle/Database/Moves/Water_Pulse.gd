3cb02a219dc86f491e8bdd6aaaf697fc787b72d1
Enerjy <55967282+Enerjy253@users.noreply.github.com>
2021-08-15 21:49:02 -0400

    Add files via upload
    -Water Pulse has a 20% chance of confusing the target. And it's power is increased by 50% when used by Pokémon with the Ability, Mega Launcher.
    -During a hailstorm, Blizzard will never miss unless the target is out of reach through a move like Dig or Fly.

---
+++ Utilities/Battle/Database/Moves/Water_Pulse.gd
@@ -0,0 +1,39 @@
+extends Object
+
+# The name of the move
+var name = "Water Pulse"
+
+# The type of the move
+var type = Type.WATER
+
+# The style of the move (Physical, Special, Status)
+var style = MoveStyle.SPECIAL
+
+# The base power of the move
+var base_power = 60
+
+# The accuracy of the move
+var accuracy = 100
+
+# The priority of the move
+var priority = 0
+
+# The critical hit level of the move 1=6.25% 2=12.5% 3=25% 4=33.3% 5=50% 
+var critical_hit_level = 1
+
+# The secondary effect chance of the move
+var secondary_effect_chance = 0.2
+
+# The secondary effect of the move
+var secondary_effect = MajorAilment.CONFUSE 
+
+# The flags of the move
+var flags = []
+
+# The total pp of the move
+var total_pp = 20
+
+# The target ability of the move (SINGLE_FOE, ALL_FOE, ALL, SELF)
+var target_ability = MoveTarget.SINGLE_FOE
+# attack, defense, sp_atack, sp_defense, speed, accuracy, evasion
+var main_status_effect 
