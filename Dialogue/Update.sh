5c5248c827cea12efa2036922f4cc399d7370dc5
ShadowTheR34l <kempcompany05@gmail.com>
2021-12-27 20:33:45 +0100

    fixed the dialogue issue

---
+++ Dialogue/Update.sh
@@ -0,0 +1,7 @@
+msginit --no-translator --input=Cutscenes.pot --locale=en --output=Generated/en/Cutscenes.po
+msginit --no-translator --input=Events.pot --locale=en --output=Generated/en/Events.po
+msginit --no-translator --input=NPC.pot --locale=en --output=Generated/en/NPC.po
+msginit --no-translator --input=Objects.pot --locale=en --output=Generated/en/Objects.po
+msginit --no-translator --input=UI.pot --locale=en --output=Generated/en/UI.po
+msginit --no-translator --input=Battle.pot --locale=en --output=Generated/en/Battle.po
+msginit --no-translator --input=Maps.pot --locale=en --output=Generated/en/Maps.po
