5399b05458328253a706d141a2405b6593cf8979
acedogblast <charles2000wang@gmail.com>
2021-12-27 19:23:13 -0500

    Add option to force touchscreen controls using -force_touchscreen_controls arg.

--- Utilities/Start.gd
+++ Utilities/Start.gd
@@ -2,7 +2,16 @@ extends Node2D
 
 #Checks if the OS is android or ios, and if it is it makes the window unable to be resized and keeps the aspec ratio
 func _ready() -> void:
-	if OS.get_name() == "Android" || OS.get_name() == "iOS":
+	var force_touchscreen_controls = false
+
+	for argument in OS.get_cmdline_args():
+		if argument.find("-force_touchscreen_controls") > -1:
+			force_touchscreen_controls = true
+			break
+			
+
+	if OS.get_name() == "Android" || OS.get_name() == "iOS" || force_touchscreen_controls:
+		Global.isMobile = true
 		OS.window_resizable = false
 		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP_HEIGHT, OS.get_real_window_size())
 		
@@ -11,6 +20,7 @@ func _ready() -> void:
 	
 	#If the above is false sets the window to be resizeable, and loads the intro scene
 	else:
+		Global.isMobile = false
 		OS.window_resizable = true
 		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, Vector2(512, 384))
 		get_tree().change_scene("res://IntroScenes/Intro.tscn")
